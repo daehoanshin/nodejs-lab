a9c255075b28da7efe233b7fbdaac9cb26b3bf07
daehoanshin <xbb123@naver.com>
2020-01-30 15:29:42 +0900

    github finder end

--- bradtraversy/Modern/githubfinder/app.js
+++ bradtraversy/Modern/githubfinder/app.js
@@ -1,5 +1,8 @@
 // Init Github
 const github = new Github;
+// Init UI
+const ui = new UI;
+
 // Search input
 const searchUser = document.getElementById('searchUser');
 
@@ -7,17 +10,19 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
   // Get input text
   const userText = e.target.value;
-
+  console.log(`userText : ${userText}`);
   if(userText !== '') {
     // Make http call
-    github.getUser()
+    github.getUser(userText)
       .then(data => {
+        
         if(data.profile.message === 'Not Found') {
           // Show alert
-
+          ui.showAlert('User not found', 'alert alert-danger');
         } else {
           // Show profile
-
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
   } else {
--- bradtraversy/Modern/githubfinder/github.js
+++ bradtraversy/Modern/githubfinder/github.js
@@ -1,15 +1,22 @@
 class Github {
   constructor() {
     this.client_id = 'Iv1.8bb8fd34c3d69326';
-    this.client_secret = 'f1eae0a10eaec7d99a5df0309d8a08ff196f530d';
+    this.client_secret = 'c4a0fae82ea05d3d8aa5d7b1e49305c2c0658eba';
+    this.repos_count = 5;
+    this.repos_sort = 'create: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+    
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
