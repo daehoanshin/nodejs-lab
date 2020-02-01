8cb95944219e06a1f9127ce42b08f1adae5ffb67
dhshin <xbb123@naver.com>
2020-02-01 21:25:25 +0900

    weather api end

--- bradtraversy/Modern/weatherjs/app.js
+++ bradtraversy/Modern/weatherjs/app.js
@@ -1,5 +1,27 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+document.getElementById('w-change-btn').addEventListener('click', e =>{
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  // Change location
+  weather.changeLocation('Miami', 'FL');
+
+  // Set location 
+  storage.setLocationData(city, state);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locaModal').modal('hide');
+});
+
 
 weather.getWeather()
   .then(results => {
