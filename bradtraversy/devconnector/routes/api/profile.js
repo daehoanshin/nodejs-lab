f517cf220bf7bbb19aadb4ba7b82f372e88929f3
daehoanshin <xbb123@naver.com>
2020-04-25 23:14:36 +0900

    section16 end

--- bradtraversy/devconnector/routes/api/profile.js
+++ bradtraversy/devconnector/routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    // only populate from user document if profile exists
+    res.json(profile.populate('user', ['name', 'avatar']));
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
