acd7da1e32220fd401cddf44fa29acc382d433ba
daehoanshin <xbb123@naver.com>
2020-09-18 16:34:16 +0900

    ellie dream coding ing

---
+++ dreamCoding/shopping-template/src/main.js
@@ -0,0 +1,16 @@
+
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+
+// main
+loadItems()
+.then(items => {
+    // displayItems(items);
+    // setEventListeners(items);
+})
+.catch(console.log);
