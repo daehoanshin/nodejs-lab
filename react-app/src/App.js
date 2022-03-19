a33a5152ebac50a0fe885b0810bed7be831eb229
daehoanshin <xbb123@naver.com>
2022-03-19 18:33:24 +0900

    [리액트js (reactjs) 기초 익히기 기본 강좌] 04 리액트 훅스 useState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
 
+  const [text, setText] = useState('Kossie');
   const onSubmit = () => {
     alert('submitted');
   };
@@ -12,13 +13,24 @@ function App() {
     } 
     console.log('key up');
   };
- 
+
+  const updateText = () => {
+    // text = 'Coder';
+    setText('Coder');
+    console.log(text);
+  }
+  console.log(text);
   return (
     <div className='App'>
       <input onKeyUp={onKeyUp} />
       <button onClick={onSubmit}>
         Submit
       </button>
+
+      <br /> <br />
+
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>
     </div>
   );
 }
